7266f6246b937b326f80f478fb8d956a060b6b1c
Bastien VINH <bus.bvinh@gmail.com>
2016-07-30 11:16:07 +0100

    Init Project

---
+++ VendingMachineLib/Exceptions/VendingMachineException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace com.bvinh.vendingmachine
+{
+
+	// This is the basic exception for our vending machine
+
+	public class VendingMachineException : Exception
+	{
+
+		public VendingMachineException(string message)
+			: base(message)
+		{
+			
+		}
+
+
+		public VendingMachineException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+			
+		}
+
+
+	}
+}
+
