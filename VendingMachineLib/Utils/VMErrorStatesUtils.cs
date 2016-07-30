15880ed9a05b4709f033873dcce0e968bcdfc0ad
Bastien VINH <bus.bvinh@gmail.com>
2016-07-30 17:56:36 +0100

    Add errors code found on applications [ADD] Add new exceptions
    [ADD] Add new errors systems, still on coding
    [COMMENT] Prepare commit for github repository : https://github.com/bastienvinh/VendingMachine

---
+++ VendingMachineLib/Utils/VMErrorStatesUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace com.bvinh.vendingmachine.utils
+{
+
+
+	// This a definition of all errors you can encounter as a Vendor Machine
+	// STOPPED                        : A vending maching can sometimes fail or be disabled by the vendor
+	// NO_ENOUGH_MONEY                : A vending must have money no matter what to give back money,
+	//																	We use it in case where the client has money and there not enough money to give back
+
+
+	// NO_MORE_DRINKS                 : There are no drinks at all in the machine
+	// RESERVE_NOT_AVAILABLE					: When a reseve doesn't have enough drinks
+	// NOT_ENOUGH_MONEY_FROM_CUSTOMER : The client doesn't have enough money to pay the vending machine
+	// CANCELED_BY_CUSTOMER						: The client has canceled a product
+
+	public enum ErrorCode
+	{
+		STOPPED = 1,
+		NO_ENOUGH_MONEY = 2,
+		NO_MORE_DRINKS = 3,
+		RESERVE_NOT_AVAILABLE = 4,
+		NOT_ENOUGH_MONEY_FROM_CUSTOMER = 5,
+		CANCELED_BY_CUSTOMER = 6
+	}
+
+	// We will use this class on the purpose to help more easily to retrieve the good errors
+
+	public static class VMErrorStatesUtils
+	{
+		
+	}
+}
+
---
+++ VendingMachineLib/VendingMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace com.bvinh.vendingmachine
+{
+
+
+	// This class represent a vending machine
+
+	public class VendingMachine
+	{
+
+
+		#region Attributes
+
+		private int _numberOfReserve = 0;
+
+		#endregion
+
+		#region Constructor
+
+		public VendingMachine(int numberOfReserve)
+		{
+		}
+
+		#endregion
+
+		#region Utils
+
+		#endregion
+	}
+}
+
