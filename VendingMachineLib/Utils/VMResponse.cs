6e4d69f4048d916135fe826e44796b8afe35e485
Bastien VINH <bus.bvinh@gmail.com>
2016-07-31 15:16:16 +0100

    Add two dfferents predefined responses

--- VendingMachineLib/Utils/VMResponse.cs
+++ VendingMachineLib/Utils/VMResponse.cs
@@ -31,6 +31,48 @@ namespace Com.Bvinh.Vendingmachine
 
 		#endregion
 
+		#region Predefined Response
+		// We use prefined response we can shorted our response.
+
+
+		/// <summary>
+		/// Create a response from Vending Machine, the client can have his product.
+		/// </summary>
+		/// <returns>The has product.</returns>
+		/// <param name="storage">Storage.</param>
+		/// <param name="p">P.</param>
+		public static VMResponse CreateHasProduct(IStorageVMProducts storage, Product p = null)
+		{
+			return new VMResponse
+			{
+				ErrorType = VMErrorCode.NONE,
+				HasError  = false,
+				HasProduct = true,
+				Product = p,
+				Storage = storage
+			};
+		}
+
+		/// <summary>
+		/// Create a predefined response from Vending Machine, there are no product anymore in this storage.
+		/// </summary>
+		/// <returns>The no product anymore.</returns>
+		/// <param name="storage">Storage.</param>
+		/// <param name="p">P.</param>
+		public static VMResponse CreateNoProductAnymore(IStorageVMProducts storage, Product p = null)
+		{
+			return new VMResponse
+			{
+				ErrorType = VMErrorCode.NO_MORE_PRODUCTS,
+				HasError = true,
+				HasProduct = false,
+				Product = p,
+				Storage = storage
+			};
+		}
+
+		#endregion
+
 	}
 }
 
