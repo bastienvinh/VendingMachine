78fbf6d009be445c54de68502490c479ad55016a
Bastien VINH <bus.bvinh@gmail.com>
2016-08-02 07:32:50 +0100

    Remove Moq package

---
+++ VendingMachineLibUnitTest/VendingMachineTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit;
+
+namespace VendingMachineLibUnitTest
+{
+	public class VendingMachineTest
+	{
+		public VendingMachineTest()
+		{
+		}
+	}
+}
+
